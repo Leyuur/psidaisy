5ac40d2b8175f15223083745cf3c5bcff7cf76b2
Leyuur <yuri.vduarte@hotmail.com>
2024-09-10 14:59:23 -0300

    mudanças

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// Configuração FireBase 
+export const firebaseConfig = {
+  apiKey: "AIzaSyCShoaRUFe6sA4zga76Ev02rqhQGD5Pxzk",
+  authDomain: "psi-daisy.firebaseapp.com",
+  projectId: "psi-daisy",
+  storageBucket: "psi-daisy.appspot.com",
+  messagingSenderId: "879694397986",
+  appId: "1:879694397986:web:4c12f9a0cba864c70b807b",
+  measurementId: "G-TE7DB8GN94"
+}
---
+++ js/index.js
@@ -0,0 +1,38 @@
+import { firebaseConfig } from "./config";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "../firebase/auth";
+
+const app = initializeApp(firebaseConfig);
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Mostrar/Ocultar senha
+    let olho = document.getElementById("olho-icon");
+    let senha = document.getElementById("senha");
+    olho.addEventListener('click', () => {
+        if (olho.innerText == "visibility") {
+            olho.innerText = "visibility_off";
+            senha.type = "password";
+        } else {
+            olho.innerText = "visibility";
+            senha.type = "text";
+        }
+    })
+
+    // Login com o google 
+    let googleBtn = document.getElementById("google-btn");
+    googleBtn.addEventListener('click', () => {
+
+        const auth = getAuth();
+        const provider = new GoogleAuthProvider();
+
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                const user = result.user;
+                console.log("Usuário logado:", user);
+            })
+            .catch((error) => {
+                console.error("Erro ao fazer login com o Google:", error);
+            });
+
+    })
+})
+
