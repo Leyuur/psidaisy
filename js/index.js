8e59a469b371a9f5f552bac6ce7b4f232459d3e6
Leyuur <yuri.vduarte@hotmail.com>
2024-10-03 12:08:31 -0300

    a

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-analytics.js";
-import { firebaseConfig } from ' https://leyuur.github.io/psidaisy/js/config.js ';
+import { firebaseConfig } from 'https://leyuur.github.io/psidaisy/js/config.js';
+
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
 
 document.addEventListener('DOMContentLoaded', () => {
     // Mostrar/Ocultar senha
@@ -20,11 +22,33 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
+        
+    // Login padrão (email/senha) 
+    document.querySelector(".form-login").addEventListener("submit", function(event) {
+        event.preventDefault();
+    
+        let userEmail = document.getElementById("email").value;
+        let userSenha = document.getElementById("senha").value;
+    
+
+        signInWithEmailAndPassword(userEmail, userSenha)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log("Usuário logado:", user);
+                alert("Bem-vindo, " + user.userEmail);
+                // window.location.href = "./menu-principal.html";
+            })
+            .catch((error) => {
+                const errorMessage = error.message;
+                console.error("Erro ao fazer login:", errorMessage);
+            });
+    });
+
+
     // Login com o google 
     let googleBtn = document.getElementById("google-btn");
     googleBtn.addEventListener('click', () => {
-
-        const auth = getAuth();
+        
         const provider = new GoogleAuthProvider();
 
         signInWithPopup(auth, provider)
